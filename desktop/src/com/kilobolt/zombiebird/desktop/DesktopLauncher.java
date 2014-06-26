dcdfa2aa915190c23ba5ef98f9ba92f6ed61958e
Vignesh Sivashanmugam <vignesh.siva@gmail.com>
2014-06-26 10:39:45 -0400

    Create Zombie Bird repository.

---
+++ desktop/src/com/kilobolt/zombiebird/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.kilobolt.zombiebird.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kilobolt.zombiebird.ZBGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Zombie Bird";
+		config.width = 480;
+		config.height = 320;
+		new LwjglApplication(new ZBGame(), config);
+	}
+}
